a5e08491f315fa5fdb405309b493a84f29864e88
echevarneluciano <82525791+echevarneluciano@users.noreply.github.com>
2023-12-12 15:35:16 -0300

    con venv

--- MKTcon/settings.py
+++ MKTcon/settings.py
@@ -9,9 +9,11 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
 from pathlib import Path
 from import_export.formats.base_formats import XLSX
+from pymysql import install_as_MySQLdb
+install_as_MySQLdb()
+
 EXPORT_FORMATS = [XLSX]
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
